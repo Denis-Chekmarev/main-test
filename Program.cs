0cbe142ae673e51e70e79158160da74eff9f018f
Denis <chekmarevdenchik5@gmail.com>
2022-06-17 13:41:06 +0700

    add clipping function

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
 ﻿
-string[] clipping(string[] arr, int numb){
-    return arr;
+string[] clipping(string[] arr, int numb)
+{
+    string[] result = new String[arr.Length];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= numb)
+        {
+            result[j] = arr[i];
+            j++;
+        }
+    }
+    return result;
 }
 
-void printArr(string[] arr){
-    for (int i=0; i < arr.Length; i++){
+void printArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
         Console.Write(arr[i] + " ");
     }
 }
 
 int number = 3;
-string[] inputArr = {"Moscow", "Add", "Check", "-2"};
+string[] inputArr = { "Moscow", "Add", "Check", "-2", "ytn", "input", "33" };
 string[] result = clipping(inputArr, number);
 printArr(result);
