cca2b57dde8d20962816fce9714cae602b609334
Denis <chekmarevdenchik5@gmail.com>
2022-06-17 13:16:21 +0700

    Create architecture of a project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] clipping(string[] arr, int numb){
+    return 0;
+}
+
+void printArr(string[] arr){
+    
+}
+
+int number = 3;
+string[] inputArr = {"Moscow", "Add", "Check", "-2"};
+string[] result = clipping(inputArr, number);
+printArr(result);
